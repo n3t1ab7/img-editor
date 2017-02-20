55f164fa11b127cc5134bdea50b3b093d20cd633
codeduan <fyyz31311@gmail.com>
2017-02-20 17:18:59 +0800

    prop

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './image-editor-dist'),
+    publicPath: '/image-editor-dist/',
     filename: 'build.js'
   },
   module: {
